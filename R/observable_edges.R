854993656689562ba2965ab2f2a22aec5ff96661
Kenneth Keuk <kenneth.keuk@gmail.com>
2020-03-09 17:43:18 +0900

    Adding option to keep track of original group scan in observable_edges()

--- R/observable_edges.R
+++ R/observable_edges.R
@@ -8,8 +8,9 @@
 #'  \item{"a dyad observation vector"}{subsetted similarly as Scan (through the non.diagonal() function for instance)}
 #'  \item{"a general dyad observation probability"}{should be in [0,1], assumed to be the case when only one value is inputed)}
 #' }
+#' @param keep logical. Indicate if the original "theoretical" scan should be kept track of.
 #'
-#' @return a similar matrix as Scan where some non diagonal edges have a probability to be NAs
+#' @return a similar matrix as Scan where some non diagonal edges have a probability to be NAs. If keep is TRUE, returns a list with theoretical and observed scan.
 #' @export
 #'
 #' @examples
@@ -21,13 +22,20 @@
 #' prob.single<- runif(1,0,1)
 #'
 #' observable_edges(Scan,prob)
-#' observable_edges(Scan,prob.single)
+#'
 
-observable_edges<- function(Scan,prob=NULL){
+observable_edges<- function(Scan,prob=NULL,keep=FALSE){
   if(is.matrix(prob)) {prob<- non.diagonal(prob,"vector")}
   if(length(prob)==1) {ifelse(prob<1 & prob>=0,prob<- rep(prob,length(non.diagonal(Scan,"vector"))),stop("Single observation probability provided should be within [0,1]"))}
 
   observable<- sapply(prob,function(p) sample(c(TRUE,FALSE),1,prob = c(p,1-p)))
-  Scan[non.diagonal(Scan)]<- ifelse(observable,non.diagonal(Scan,"vector"),NA)
-  Scan
+
+  observed<- Scan;
+  observed[non.diagonal(observed)]<- ifelse(observable,non.diagonal(Scan,"vector"),NA)
+  if(keep){
+    list("theoretical" = Scan,
+         "observed" = observed)
+  }else{
+    observed
+  }
 }
