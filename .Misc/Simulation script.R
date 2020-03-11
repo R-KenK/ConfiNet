d211f5bc9f29f666ba83b949930a4a38531af2db
Kenneth Keuk <kenneth.keuk@gmail.com>
2020-03-11 20:27:41 +0900

    prototype for integrating analysis to the script

--- .Misc/Simulation script.R
+++ .Misc/Simulation script.R
@@ -18,7 +18,7 @@ set.seed(42)
 
 n<- 10;nodes<- letters[1:n];
 total_scan<- 100; #from original paper
-n.boot<- 50;
+n.boot<- 5;
 
 Adj<- matrix(data = 0,nrow = n,ncol = n,dimnames = list(nodes,nodes))
 Adj[non.diagonal(Adj)]<- sample((0:round(total_scan*.50)),n*(n-1),replace = TRUE)
@@ -36,11 +36,58 @@ FOCAL.LIST<- list(random = sample(nodes,total_scan,replace = TRUE),
                   biased = "TO IMPLEMENT")
 
 # Boot_scans() wrapper to loop through parameters -------------------------
+#' Title
+#'
+#' @param Adj
+#' @param n.obs
+#' @param total_scan
+#' @param obs.prob
+#' @param mode
+#' @param focal.list
+#'
+#' @return
+#' @export
+#'
+#' @examples
 Boot_with.parameter.list<- function(Adj,n.obs,total_scan,obs.prob,mode,focal.list){
   cat(paste0("obs.prob = ",obs.prob," - focal.list = ",names(focal.list)," - mode = ",mode,"\n"))
   Boot_scans(Adj = Adj,n.boot = n.boot,total_scan = total_scan,obs.prob = 0.6,keep = TRUE,
              method = "both",focal.list = focal.list[[1]],scaled = TRUE,mode = "directed",output = "all",n.cores = 7)
 }
+
+#' Title
+#'
+#' @param Bootstrap
+#' @param n.boot
+#' @param what
+#'
+#' @return
+#' @export
+#'
+#' @examples
+adjacency_cor<- function(Bootstrap,what = c("observed","focal"),n.boot = length(Bootstrap)){
+  what<- match.arg(what)
+  sapply(1:n.boot,  # needs function to gather and structure in a data frame
+         function(b) {
+           cor(c(Boot_get.list(Bootstrap,"theoretical")$adjacency[[b]]),   # c() flattens the matrix to consider it like a vector
+               c(Boot_get.list(Bootstrap,what)$adjacency[[b]]))
+         }
+  )
+}
+
+#' Title
+#'
+#' @param X
+#' @param FUN
+#'
+#' @return
+#' @export
+#'
+#' @examples
+rbind_lapply<- function(X,FUN){
+  do.call(rbind,lapply(X = X,FUN = FUN))
+}
+
 start<- Sys.time()
 Bootstrap.list<- lapply(seq_along(OBS.PROB),
                         function(param.obs){
@@ -48,10 +95,18 @@ Bootstrap.list<- lapply(seq_along(OBS.PROB),
                                  function(param.foc){
                                    lapply(seq_along(MODE),
                                           function(param.mode){
-                                            Boot_with.parameter.list(Adj,n.obs,total_scan,
-                                                                     obs.prob = OBS.PROB[[param.obs]],
-                                                                     mode = MODE[[param.mode]],
-                                                                     focal.list = FOCAL.LIST[param.foc] )
+                                            Bootstrap<- Boot_with.parameter.list(Adj,n.obs,total_scan,
+                                                                                 obs.prob = OBS.PROB[[param.obs]],
+                                                                                 mode = MODE[[param.mode]],
+                                                                                 focal.list = FOCAL.LIST[param.foc])
+                                            list(Bootstrap = Bootstrap,
+                                                 stats = data.table::data.table(obs.prob = OBS.PROB[[param.obs]],
+                                                                                focal.list = as.factor(names(FOCAL.LIST[param.foc])),
+                                                                                mode = as.factor(MODE[[param.mode]]),
+                                                                                group.cor = adjacency_cor(Bootstrap,what = "observed"),
+                                                                                focal.cor = adjacency_cor(Bootstrap,what = "focal")
+                                                 )
+                                            )
                                           }
                                    )
                                  }
@@ -59,17 +114,28 @@ Bootstrap.list<- lapply(seq_along(OBS.PROB),
                         }
 )
 stop<- Sys.time()
+stop-start
 
-# sapply(1:n.boot,
-#        function(b) {
-#          cor(c(Boot_get.list(test,"theoretical")$adjacency[[b]]),   # c() flattens the matrix to consider it like a vector
-#              c(Boot_get.list(test,"observed")$adjacency[[b]]))
-#        }
-# )
-#
-# sapply(1:n.boot,
-#        function(b) {
-#          cor(c(Boot_get.list(test,"theoretical")$adjacency[[b]]),
-#              c(Boot_get.list(test,"focal")$adjacency[[b]]))
-#        }
-# )
+data<- rbind_lapply(Bootstrap.list,
+                    function(param.obs){
+                      rbind_lapply(param.obs,
+                                   function(param.foc){
+                                     rbind_lapply(param.foc,
+                                                  function(L){
+                                                    L$"stats"
+                                                  }
+                                     )
+                                   }
+                      )
+                    }
+)
+
+library(ggplot2)
+library(data.table)
+data.long<- data.table(reshape2::melt(data,id.vars = 1:3,measure.vars = 4:5,variable.name = "method",value.name = "cor"))
+data.summary<- data.long[,.(cor=median(cor),sd=sd(cor)),by = .(obs.prob,focal.list,mode,method)]
+ggplot(data.long,aes(interaction(method,obs.prob),cor,colour = method))+facet_grid(mode~focal.list)+geom_jitter(alpha=0.2)+geom_boxplot()+theme_bw()
+ggplot(data.summary,aes(interaction(method,obs.prob),cor,fill = method))+facet_grid(mode~focal.list)+
+  geom_errorbar(aes(ymin = cor-sd,ymax=cor+sd),width = 0.2)+geom_bar(stat = "identity")+theme_bw()
+
+str(Bootstrap.list)
