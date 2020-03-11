2a0797ce4c3049e7ead42ae34bc48c424eb97f7e
Kenneth Keuk <kenneth.keuk@gmail.com>
2020-03-11 16:34:07 +0900

    Simulation script proof of concept with few parameters candidates

--- .Misc/Simulation script.R
+++ .Misc/Simulation script.R
@@ -12,15 +12,14 @@ focal.list<- sample(row.names(Adj),1138,replace = TRUE)
 table(focal.list)
 
 test<- ConfiNet::Boot_scans(Adj = Adj,n.boot = 100,total_scan = total_scan,
-                     method = "both",focal.list = focal.list,scaled = TRUE,mode = "max",output = "adj",n.cores = 7)
+                            method = "both",focal.list = focal.list,scaled = TRUE,mode = "max",output = "adj",n.cores = 7)
 head(test)
 
 
 
+# Simulation script backbone ----------------------------------------------
 
-
-# progress bar testing ----------------------------------------------------
-
+## import needed function, but ultimately should use library(ConfiNet) --------
 source("R/matrix.tools.R")
 source("R/Binary.prob.R")
 source("R/do.scan.R")
@@ -30,28 +29,63 @@ source("R/Boot_scans.R")
 source("R/observable_edges.R")
 source("R/Bootstrap_tools.R")
 
-G<- igraph::read_graph("C:/R/Git/asnr/Networks/Mammalia/rhesusmacaque_association_weighted/weighted_Contact_Sits_Macaque.graphml",format = "graphml")
-total_scan<- 1138;n.boot<- 100;
-Adj<- as.matrix(igraph::as_adj(G,attr = "weight"))
-row.names(Adj)<- as.character(1:nrow(Adj));colnames(Adj)<- row.names(Adj)
-do.scan(Adj=Adj,total_scan=total_scan,mode = "max")
 
-focal.list<- sample(row.names(Adj),total_scan,replace = TRUE)
+# import and generate objects ---------------------------------------------
+set.seed(42)
+
+n<- 10;nodes<- letters[1:n];
+total_scan<- 100; #from original paper
+n.boot<- 50;
+
+Adj<- matrix(data = 0,nrow = n,ncol = n,dimnames = list(nodes,nodes))
+Adj[non.diagonal(Adj)]<- sample((0:round(total_scan*.50)),n*(n-1),replace = TRUE)
+Adj
+
+focal.list<- sample(nodes,total_scan,replace = TRUE)
 table(focal.list)
 
-Bootstrap<- Boot_scans(Adj = Adj,n.boot = n.boot,total_scan = total_scan,obs.prob = 0.6,keep = TRUE,
-                  method = "both",focal.list = focal.list,scaled = TRUE,mode = "directed",output = "all",n.cores = 7)
 
-sapply(1:n.boot,
-       function(b) {
-         cor(c(Boot_get.list(test,"theoretical")$adjacency[[b]]),   # c() flattens the matrix to consider it like a vector
-             c(Boot_get.list(test,"observed")$adjacency[[b]]))
-       }
-)
+# Parameter choices -------------------------------------------------------
+OBS.PROB<- seq(0.1,0.9,by = 0.2)
+MODE<- c("directed","max")
+FOCAL.LIST<- list(random = sample(nodes,total_scan,replace = TRUE),
+                  even = rep_len(nodes,length.out = total_scan),
+                  biased = "TO IMPLEMENT")
 
-sapply(1:n.boot,
-       function(b) {
-         cor(c(Boot_get.list(test,"theoretical")$adjacency[[b]]),
-             c(Boot_get.list(test,"focal")$adjacency[[b]]))
-       }
+# Boot_scans() wrapper to loop through parameters -------------------------
+Boot_with.parameter.list<- function(Adj,n.obs,total_scan,obs.prob,mode,focal.list){
+  Boot_scans(Adj = Adj,n.boot = n.boot,total_scan = total_scan,obs.prob = 0.6,keep = TRUE,
+             method = "both",focal.list = focal.list,scaled = TRUE,mode = "directed",output = "all",n.cores = 7)
+}
+start<- Sys.time()
+Bootstrap.list<- lapply(seq_along(OBS.PROB),
+                        function(param.obs){
+                          lapply(seq_along(FOCAL.LIST[1:2]),
+                                 function(param.foc){
+                                   lapply(seq_along(MODE),
+                                          function(param.mode){
+                                            Boot_with.parameter.list(Adj,n.obs,total_scan,
+                                                                     obs.prob = OBS.PROB[[param.obs]],
+                                                                     mode = MODE[[param.mode]],
+                                                                     focal.list = FOCAL.LIST )
+                                          }
+                                   )
+                                 }
+                          )
+                        }
 )
+stop<- Sys.time()
+
+# sapply(1:n.boot,
+#        function(b) {
+#          cor(c(Boot_get.list(test,"theoretical")$adjacency[[b]]),   # c() flattens the matrix to consider it like a vector
+#              c(Boot_get.list(test,"observed")$adjacency[[b]]))
+#        }
+# )
+#
+# sapply(1:n.boot,
+#        function(b) {
+#          cor(c(Boot_get.list(test,"theoretical")$adjacency[[b]]),
+#              c(Boot_get.list(test,"focal")$adjacency[[b]]))
+#        }
+# )
