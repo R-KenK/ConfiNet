5abe5e62666b77e24e8d6e14169f21d05452ea8c
Kenneth Keuk <kenneth.keuk@gmail.com>
2020-04-26 14:54:36 +0900

    revamped Optimization for rare event script to benchmark and compare actual implementation of optimization in Boot_scan()

--- .WIP/Optimization for rare events.R
+++ .WIP/Optimization for rare events.R
@@ -1,241 +1,146 @@
 # Draft of optimization for rare events ----------------------------------------------------------
+rm(list = ls(envir = .GlobalEnv))
 set.seed(42)
 
 ## import needed function, but ultimately should use library(ConfiNet) --------
 source("R/matrix.tools.R")
+source('R/rare.event.optimization_tools.R')
+source("R/Misc_tools.R")
 source("R/Binary.prob.R")
+source("R/do.scan.R")
+source("R/sum_up.scans.R")
+source("R/iterate_scans.R")
+source("R/Boot_scans.R")
+source("R/observable_edges.R")
 source("R/Bootstrap_tools.R")
-library(data.table)
-library(microbenchmark)
+source(".WIP/ASNR.tools.R")
 
-#' Title
-#'
-#' @param P
-#' @param ...
-#' @param n
-#'
-#' @return
-#' @export
-#'
-#' @examples
-P.cond.in.order<- function(P,...,n = length(P)){
-  S<- 1-prod(1-P)
-  previous.are.zeros<- c(1,cumprod(1-P[1:(n-1)]))
-  sapply(1:n,function(i) P[i]/S*previous.are.zeros[i])
+Opti.test.Boot_scan.to.adj_wrapper<- function(Adj,total_scan,max.obs,method,use.rare.opti){
+  Bootstrap<- Boot_scans(Adj = Adj,total_scan = total_scan,method = method,n.boot = 1,obs.prob = 0.42,scaled = TRUE,output = "all",use.rare.opti = use.rare.opti,cl = NULL)
+  non.diagonal(Boot_get.list(Bootstrap,what = "theoretical",get.format = "adjacency")[[1]],output = "vector")
 }
 
-#' Title
+#' Optimization empirical support bootstrap function
+#' Perform a bootstrap to estimate the coefficient of correlation and time of calculation of the theoretical networks with or without the optimization for rare event, and for given n, total_scan and max.obs. Parameters of Boot_scans() are set to do only one network estimation, but with the most demanding parameters(scaled = TRUE, method = "both", output = "all", mode, obs.prob and focal.list to be set by scan.default.args)
 #'
-#' @param P
-#' @param ...
-#' @param n
+#' @param n number of nodes in a simulated original network, to be approximated through iterating binary scans.
+#' @param total_scan sampling effort, i.e. number of binary scan to perform
+#' @param max.obs maximum number of observation possible within the simulated original network
+#' @param boot number of bootstrap to perform (for the coefficient of correlation and time measure)
 #'
-#' @return
+#' @return a data.table with the coefficient of correlation and (median) time of calculation of the theoretical networks, obtained through the Boot_scan() function, with or without the optimization for rare event.
 #' @export
 #'
 #' @examples
-do.non.zero.scan<-function(P,...,n = length(P)){
-  X<- rep(0,n)
-  P.cond<- cumsum(P.cond.in.order(P))
-  first<- min(which(runif(1)<P.cond))
-  X[first]<- 1
-  X[-first]<- rbinom(n-1,1,P[-first])
-  X
-}
-
-#' Title
-#'
-#' @param P
-#' @param ...
-#' @param n
-#'
-#' @return
-#' @export
-#'
-#' @examples
-do.non.zero.scan.bis<-function(P,...,n = length(P)){
-  X<- rep(0,n)
-  rand.order<- sample(1:n,n)
-  P.cond<- cumsum(P.cond.in.order(P[rand.order]))
-  first<- min(which(runif(1)<P.cond))
-  X[rand.order][first]<- 1
-  X[rand.order][-first]<- rbinom(n-1,1,P[rand.order][-first])
-  X
-}
-
-#' Title
-#'
-#' @param P
-#' @param N
-#' @param ...
-#' @param n
-#'
-#' @return
-#' @export
-#'
-#' @examples
-iterate_rare.scans<- function(P,N,...,n = length(P)){
-  scan<- data.table::data.table(matrix(0,N,n));
-  non.zero<- rbinom(N,1,1-prod(1-P))==1;
-  scan[non.zero,]<- data.table::data.table(rbind_lapply(seq_len(nrow(scan[non.zero,])),function(k) do.non.zero.scan(P)))
-  scan
-}
-
-
-
-#' Title
-#'
-#' @param P
-#' @param N
-#' @param ...
-#' @param n
-#'
-#' @return
-#' @export
-#'
-#' @examples
-iterate_standard.scans<- function(P,N,...,n = length(P)){
-  rbind_lapply(1:N,
-               function(k){
-                 X<- rbinom(n,1,P)
-                 if(Reduce("&",X==0)) rep(0,n) else X
-               }
-  )
-}
-
-#' Title
-#'
-#' @param n
-#' @param N
-#' @param max.obs
-#' @param boot
-#'
-#' @return
-#' @export
-#'
-#' @examples
-cor.bootstrap<- function(n,N,max.obs,boot=100,n.cores=1,.export){
-  V<- round(runif(n,0,max.obs))
-  P<- Binary.prob(V,N,mode = "vector")$present
-  rbind_pblapply(1:boot,
+#' # internal use, cf hereafter.
+cor.bootstrap<- function(n,total_scan,max.obs,boot=100){
+  nodes<- as.character(1:n)
+  Adj<- matrix(round(runif(n*n,0,max.obs)),nrow = n,ncol = n,dimnames = list(nodes,nodes))
+  diag(Adj)<- 0
+  rbind_lapply(1:boot,
                  function(b){
-                   time<- microbenchmark(
-                     standard = {X<- iterate_standard.scans(P,N)},
-                     opti.ordered = {X.opti<- iterate_rare.scans(P,N)},
-                     opti.random = {X.opti.bis<- iterate_rare.scans.bis(P,N)},
+                   time<- microbenchmark::microbenchmark(
+                     # standard.group = {Adj.std.group<- Opti.test.Boot_scan.to.adj_wrapper(Adj = Adj,total_scan = total_scan,max.obs = max.obs,method = "group",use.rare.opti = FALSE)},
+                     # opti.group = {Adj.opti.group<- Opti.test.Boot_scan.to.adj_wrapper(Adj = Adj,total_scan = total_scan,max.obs = max.obs,method = "group",use.rare.opti = TRUE)},
+                     # standard.focal = {Adj.std.focal<- Opti.test.Boot_scan.to.adj_wrapper(Adj = Adj,total_scan = total_scan,max.obs = max.obs,method = "focal",use.rare.opti = FALSE)},
+                     # opti.focal = {Adj.opti.focal<- Opti.test.Boot_scan.to.adj_wrapper(Adj = Adj,total_scan = total_scan,max.obs = max.obs,method = "focal",use.rare.opti = TRUE)},
+                     standard.all = {Adj.std.all<- Opti.test.Boot_scan.to.adj_wrapper(Adj = Adj,total_scan = total_scan,max.obs = max.obs,method = "both",use.rare.opti = FALSE)},
+                     opti.all = {Adj.opti.all<- Opti.test.Boot_scan.to.adj_wrapper(Adj = Adj,total_scan = total_scan,max.obs = max.obs,method = "both",use.rare.opti = TRUE)},
                      times = 1,unit = "ms"
                    )
                    time
 
-                   cor<- data.table(n=n,N=N,max.obs=max.obs,method=c("standard","opti.ordered","opti.random"),
-                                    cor=c(cor(colSums(X),V),cor(colSums(X.opti),V),cor(colSums(X.opti.bis),V)))
+
+
+                   cor<- data.table::data.table(n = n,total_scan = total_scan,max.obs = max.obs,
+                                                algorithm = rep_len(c("standard","opti"),length.out = 2),
+                                                method = "all",
+                                                # method = rep(c("group","focal","both"),each = 2),
+                                                cor=c(
+                                                  # cor(non.diagonal(Adj,output = "vector"),Adj.std.group),
+                                                  # cor(non.diagonal(Adj,output = "vector"),Adj.opti.group),
+                                                  # cor(non.diagonal(Adj,output = "vector"),Adj.std.focal),
+                                                  # cor(non.diagonal(Adj,output = "vector"),Adj.opti.focal),
+                                                  cor(non.diagonal(Adj,output = "vector"),Adj.std.all),
+                                                  cor(non.diagonal(Adj,output = "vector"),Adj.opti.all)
+                                                )
+                   )
+
 
                    cbind(cor,time=summary(time)[["median"]],boot=b)
-                 },n.cores = n.cores,.export = .export
+                 }
   )
 }
 
 
 
 # Parameters list ---------------------------------------------------------
-param.comb<- expand.grid(n=c(10,50,100,150,200,500,1000),
-                         N=c(500,1000,10000,50000),
+param.comb<- expand.grid(n=c(10,50,100,150,200,500,1000,1500),
+                         total_scan=c(500,1000,5000,10000,25000),
                          max.obs=c(5,10,50,100,250))
 
 parameters.list<- lapply(1:nrow(param.comb),
                          function(p){
                            c(n=param.comb[p,]$n,
-                             N=param.comb[p,]$N,
+                             total_scan=param.comb[p,]$total_scan,
                              max.obs=param.comb[p,]$max.obs)
                          }
 )
 
-
 # Validation (and comparison) of the optimizations ------------------------------------------
-## Run standard method of doing a scan for each i of 1:N, as well as optimized ones where only scans are performed when the scan is expected to be non full-zero
+## Run standard method of doing a scan for each i of 1:total_scan, as well as optimized ones where only scans are performed when the scan is expected to be non full-zero
 ## then record coefficient of correlation of cumulated sum of the bernouilli trials with original vector of observations
 ## as well as the time to perform each method
 
-cor.boot<- rbind_lapply(seq_along(parameters.list),
-                        function(p){
-                          n<- parameters.list[[p]]["n"];
-                          N<- parameters.list[[p]]["N"];
-                          max.obs<- parameters.list[[p]]["max.obs"];
-                          cat(paste0("Param: n= ",n," - N= ",N," - max.obs= ",max.obs," (",p,"/",length(parameters.list),")\n"))
-                          cor.bootstrap(n,N,max.obs,boot = 100,n.cores = 7,
-                                        .export = c("parameters.list","cor.bootstrap",
-                                                    "iterate_standard.scans","iterate_rare.scans","iterate_rare.scans.bis",
-                                                    "do.non.zero.scan","do.non.zero.scan.bis","P.cond.in.order",
-                                                    "Binary.prob","rbind_lapply","data.table","microbenchmark"))
-                        }
-
+cl<- snow::makeCluster(7);snow::clusterExport(cl = cl,list = ls(envir = .GlobalEnv));doSNOW::registerDoSNOW(cl)
+system.time(
+  cor.boot<- rbind_pblapply(seq_along(parameters.list),
+                          function(p){
+                            n<- parameters.list[[p]]["n"];
+                            total_scan<- parameters.list[[p]]["total_scan"];
+                            max.obs<- parameters.list[[p]]["max.obs"];
+                            cat(paste0("Param: n= ",n," - total_scan= ",total_scan," - max.obs= ",max.obs," (",p,"/",length(parameters.list),") @ ",Sys.time(),"\n"))
+                            cor.bootstrap(n = n,total_scan = total_scan,max.obs = max.obs,boot = 30)
+                          },cl = cl
+  )
 )
-
-cor.boot<- cor.boot[order(method)]
-cor.boot$method<- factor(cor.boot$method,levels = c("standard","opti.ordered","opti.random"))
+snow::stopCluster(cl)
+cor.boot<- cor.boot[order(algorithm)][,c("algorithm","n","total_scan","max.obs","method","cor","time","boot"),with=FALSE]
+cor.boot$algorithm<- factor(cor.boot$algorithm,levels = c("standard","opti"))
+cor.boot$method<- factor(cor.boot$method,levels = c("group","focal","both"))
 
 # saveRDS(cor.boot,".WIP/cor.boot.rds")
 # cor.boot<- readRDS(".WIP/cor.boot.rds")
 
 library(ggplot2)
 library(data.table)
-ggplot(cor.boot,aes(interaction(method,N),cor,colour=method))+
+ggplot(cor.boot,aes(interaction(algorithm,total_scan),cor,colour=algorithm))+
   facet_grid(.~max.obs)+
   geom_jitter(alpha=0.2)+geom_boxplot(alpha=0.8)+theme_bw()
 
-cor.summary<- cor.boot[,.(cor=median(cor),perc.5=quantile(cor,.05),perc.95=quantile(cor,.95)),by=.(method,n,N,max.obs)]
-cor.summary$method<- relevel(factor(cor.summary$method),"standard")
-cor.summary$N.text<- factor(paste0("N = ",cor.summary$N),levels = paste0("N = ",unique(cor.summary$N)))
+cor.summary<- cor.boot[,.(cor=median(cor),perc.5=quantile(cor,.05),perc.95=quantile(cor,.95)),by=.(algorithm,n,total_scan,max.obs)]
+cor.summary$algorithm<- relevel(factor(cor.summary$algorithm),"standard")
+cor.summary$total_scan.text<- factor(paste0("total_scan = ",cor.summary$total_scan),levels = paste0("total_scan = ",unique(cor.summary$total_scan)))
 cor.summary$max.obs.text<- factor(paste0("max.obs = ",cor.summary$max.obs),levels = paste0("max.obs = ",unique(cor.summary$max.obs)))
-cor.plot<- ggplot(cor.summary,aes(n,cor,colour=method,fill=method,group=method))+
-  facet_wrap(N.text~max.obs.text,scales = "free")+
+cor.plot<- ggplot(cor.summary,aes(n,cor,colour=algorithm,fill=algorithm,group=algorithm))+
+  facet_wrap(total_scan.text~max.obs.text,scales = "free")+
   geom_ribbon(aes(ymin = perc.5,ymax = perc.95),colour=NA,alpha=0.3)+
   geom_line()+geom_point(alpha=1)+theme_bw()
 cor.plot
-ggplot(cor.summary,aes(n,cor,colour=method,fill=method,group=method))+
-  facet_wrap(N~max.obs,scales = "free")+
+ggplot(cor.summary,aes(n,cor,colour=algorithm,fill=algorithm,group=algorithm))+
+  facet_wrap(total_scan~max.obs,scales = "free")+
   geom_linerange(aes(ymin = perc.5,ymax = perc.95),alpha=1,position = position_dodge(50/3-1))+
   geom_line(position = position_dodge(50/3-1))+geom_point(alpha=1,position = position_dodge(50/3-1))+theme_bw()
 ggsave(filename = ".WIP/optimization_cor.plot.pdf",plot = cor.plot,width = 20,height = 15,units = "in")
 
-cor.summary<- cor.boot[,.(cor=median(cor),perc.5=quantile(cor,.05),perc.95=quantile(cor,.95)),by=.(method,n,N,max.obs)]
-cor.summary<- cor.summary[method!="opti.ordered"]
-cor.summary$method<- ifelse(cor.summary$method=="standard","standard","opti")
-cor.summary$method<- relevel(factor(cor.summary$method),"standard")
-cor.summary$N.text<- factor(paste0("N = ",cor.summary$N),levels = paste0("N = ",unique(cor.summary$N)))
-cor.summary$max.obs.text<- factor(paste0("max.obs = ",cor.summary$max.obs),levels = paste0("max.obs = ",unique(cor.summary$max.obs)))
-cor.plot<- ggplot(cor.summary,aes(n,cor,colour=method,fill=method,group=method))+
-  facet_wrap(N.text~max.obs.text,scales = "free")+
-  geom_ribbon(aes(ymin = perc.5,ymax = perc.95),colour=NA,alpha=0.3)+
-  geom_line()+geom_point(alpha=1)+theme_bw()
-cor.plot
-ggsave(filename = ".WIP/optimization_cor.plot.standard.vs.opti.pdf",plot = cor.plot,width = 20,height = 15,units = "in")
 
-time.summary<- time.boot[,.(time=median(time),perc.5=quantile(time,.05),perc.95=quantile(time,.95)),by=.(method,n,N,max.obs)]
-time.summary$method<- relevel(factor(time.summary$method),"standard")
-time.summary$N.text<- factor(paste0("N = ",time.summary$N),levels = paste0("N = ",unique(time.summary$N)))
+time.summary<- cor.boot[,.(time=median(time),perc.5=quantile(time,.05),perc.95=quantile(time,.95)),by=.(algorithm,n,total_scan,max.obs)]
+time.summary$algorithm<- relevel(factor(time.summary$algorithm),"standard")
+time.summary$total_scan.text<- factor(paste0("total_scan = ",time.summary$total_scan),levels = paste0("total_scan = ",unique(time.summary$total_scan)))
 time.summary$max.obs.text<- factor(paste0("max.obs = ",time.summary$max.obs),levels = paste0("max.obs = ",unique(time.summary$max.obs)))
-time.plot<- ggplot(time.summary,aes(n,time,colour=method,fill=method,group=method))+
-  facet_wrap(N.text~max.obs.text,scales = "free")+
+time.plot<- ggplot(time.summary,aes(n,time,colour=algorithm,fill=algorithm,group=algorithm))+
+  facet_wrap(total_scan.text~max.obs.text,scales = "free")+
   geom_ribbon(aes(ymin = perc.5,ymax = perc.95),colour=NA,alpha=0.3)+
   geom_line()+geom_point(alpha=1)+theme_bw()
 time.plot
 ggsave(filename = ".WIP/optimization_time.plot.pdf",plot = time.plot,width = 20,height = 15,units = "in")
-ggplot(time.summary,aes(n,time,colour=method,fill=method,group=method))+
-  facet_grid(N~max.obs)+
-  geom_linerange(aes(ymin = perc.5,ymax = perc.95),alpha=1,position = position_dodge(50/3-1))+
-  geom_line(position = position_dodge(50/3-1))+geom_point(alpha=1,position = position_dodge(50/3-1))+theme_bw()
-
-time.summary<- time.summary[method!="opti.ordered"]
-time.summary$method<- ifelse(time.summary$method=="standard","standard","opti")
-time.summary$method<- relevel(factor(time.summary$method),"standard")
-time.summary$N.text<- factor(paste0("N = ",time.summary$N),levels = paste0("N = ",unique(time.summary$N)))
-time.summary$max.obs.text<- factor(paste0("max.obs = ",time.summary$max.obs),levels = paste0("max.obs = ",unique(time.summary$max.obs)))
-time.plot<- ggplot(time.summary,aes(n,time,colour=method,fill=method,group=method))+
-  facet_wrap(N.text~max.obs.text,scales = "free")+
-  geom_ribbon(aes(ymin = perc.5,ymax = perc.95),colour=NA,alpha=0.3)+
-  geom_line()+geom_point(alpha=1)+theme_bw()
-time.plot
-ggsave(filename = ".WIP/optimization_time.plot.standard.vs.opti.pdf",plot = time.plot,width = 20,height = 15,units = "in")
-
-paste0("N = ",unique(time.summary$N))
